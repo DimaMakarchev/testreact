e0307430f325a9ecca6f211ebdce1da780729d07
DimaMakarchev <49458189+DimaMakarchev@users.noreply.github.com>
2021-06-07 10:46:28 +0300

    commit bro

---
+++ src/test/java/com/testreact/testreact/TestreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testreact.testreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
